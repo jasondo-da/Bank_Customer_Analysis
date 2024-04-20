c1f24b383cb27dd509063db27dff4879f39948a4
Jason Do <138195365+jasondo-da@users.noreply.github.com>
2024-04-19 23:34:50 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,16 @@
+****************************
+-- Bank Customer Analysis --
+****************************
+
+	
+----------------
+-- Schema SQL --
+----------------
+	
+/* Creating schema for project */
+CREATE SCHEMA bank_customers
+
+	
 /* Creating a table with more efficient data types for analysis */
 CREATE TABLE bank_customers.customer_profile (
   row_id SMALLINT UNSIGNED,
@@ -22,6 +35,7 @@ CREATE TABLE bank_customers.customer_profile (
   CONSTRAINT unique_id PRIMARY KEY (customer_id)
 )
 
+	
 /* Importing data into new table with more efficient data types */
 INSERT INTO bank_customers.customer_profile (
 	row_id,
@@ -64,10 +78,46 @@ SELECT RowNumber,
 	PointEarned
 FROM customer_churn_records
 
+	
 /* Cleaning data for consistent formatting */
 UPDATE customer_profile
 SET sur_name = LOWER(sur_name),
 	geography = LOWER(geography)
 WHERE customer_id > 0
+
+	
+---------------
+-- Query SQL --
+---------------
+
+/* Finding the customer tenure distribution */
+SELECT tenure, COUNT(tenure)
+FROM customer_profile
+GROUP BY tenure
+ORDER BY tenure
+
+	
+/* Uncovering customer age distribution */
+SELECT FLOOR(
+	(CASE WHEN age = mm.max_age
+		THEN mm.max_age * 0.999999999 
+		ELSE age 
+		END - mm.min_age
+	) / (
+		mm.max_age - mm.min_age
+		) * 10
+		) + 1 cohort,
+	MIN(age) min_age,
+	MAX(age) max_age,
+    	COUNT(age) count
+FROM customer_profile
+CROSS JOIN (
+    SELECT MAX(age) max_age,
+        MIN(age) min_age
+    FROM customer_profile) mm
+WHERE age IS NOT NULL
+GROUP BY cohort
+ORDER BY cohort
+
 
 /*  */
